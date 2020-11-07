19c7f59e1c72d633605b8a70b5ba85c675a278ef
Doron Marcus <doron.marcus@gmail.com>
2020-11-07 16:14:09 +0200

    make modal general

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,11 +1,27 @@
 import React from "react";
 import Modal from "../Modal";
+import history from "../../history";
 
 const StreamDelete = () => {
+    //* React Fragment is a wrapper that does not have impact on dom
+    const actions = (
+        <React.Fragment>
+            <button className="ui button negative">Delete</button>
+            <button className="ui button">Cancel</button>
+        </React.Fragment>
+    );
+
     return (
         <div>
             StreamDelete
-            <Modal />
+            <Modal
+                title="Delete Stream"
+                content="Are you sure you want to delete this stream?"
+                actions={actions}
+                onDismiss={() => {
+                    history.push("/");
+                }}
+            />
         </div>
     );
 };
