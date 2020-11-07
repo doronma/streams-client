ab486d617f22a7be9a07979c500c0cc56864f05c
Doron Marcus <doron.marcus@gmail.com>
2020-11-07 17:00:53 +0200

    handle delete stream

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,29 +1,59 @@
+import { connect } from "react-redux";
 import React from "react";
+import { Link } from "react-router-dom";
 import Modal from "../Modal";
 import history from "../../history";
 
-const StreamDelete = () => {
-    //* React Fragment is a wrapper that does not have impact on dom
-    const actions = (
-        <React.Fragment>
-            <button className="ui button negative">Delete</button>
-            <button className="ui button">Cancel</button>
-        </React.Fragment>
-    );
+import { fetchStream, deleteStream } from "../../actions";
 
-    return (
-        <div>
-            StreamDelete
+class StreamDelete extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    //* React Fragment is a wrapper that does not have impact on dom
+    renderActions() {
+        const { id } = this.props.match.params;
+        return (
+            <React.Fragment>
+                <button
+                    className="ui button negative"
+                    onClick={() => this.props.deleteStream(id)}
+                >
+                    Delete
+                </button>
+                <Link className="ui button" to="/">
+                    Cancel
+                </Link>
+            </React.Fragment>
+        );
+    }
+
+    renderContent() {
+        if (!this.props.stream) {
+            return "Are you sure you want to delete this stream?";
+        }
+        return `Are you sure you want to delete stream with title: ${this.props.stream.title}`;
+    }
+
+    render() {
+        return (
             <Modal
                 title="Delete Stream"
-                content="Are you sure you want to delete this stream?"
-                actions={actions}
+                content={this.renderContent()}
+                actions={this.renderActions()}
                 onDismiss={() => {
                     history.push("/");
                 }}
             />
-        </div>
-    );
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamDelete;
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+    StreamDelete
+);
